1a1c3462a0c71b2197cc8932623e557fe6eb142a
Maxime Caspani <maxime.caspani@natixis.com>
2021-07-13 14:16:26 +0200

    Quest 623 Completed

--- Thanos.java
+++ Thanos.java
@@ -1,3 +1,6 @@
+import java.util.Comparator;
+import java.util.TreeMap;
+
 public class Thanos {
 
     public static void main(String[] args) {
@@ -17,14 +20,31 @@ public class Thanos {
         Flower rose = new Flower("Rose");
 
         // TODO 1 : Create a TreeMap where :
+        Comparator<Hero> comparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero orig, Hero compare) {
+                return orig.getName().compareTo(compare.getName());
+            }
+        };
         // Hulk, Thor and Scarlet Witch have a Rose
         // Vision has a Tulip
         // Captain America has a Lily
         // Doctor Strange and Black Widow have a Violet
+        TreeMap<Hero, Flower> heroesFavFlowers = new TreeMap<>(comparator);
+        heroesFavFlowers.put(hulk, rose);
+        heroesFavFlowers.put(scarletWitch, rose);
+        heroesFavFlowers.put(thor, rose);
+        heroesFavFlowers.put(vision, tulip);
+        heroesFavFlowers.put(captainAmerica, lily);
+        heroesFavFlowers.put(blackWidow, violet);
+        heroesFavFlowers.put(doctorStrange, violet);
 
         // TODO 2 : Print if `begonia` is contained in the TreeMap
+        System.out.println(heroesFavFlowers.containsValue(begonia));
 
         // TODO 3 : For each hero, alphabetically, print the corresponding flower
-
+        for (Flower flower : heroesFavFlowers.values()){
+            System.out.println(flower.getName());
+        }
     }
 }
